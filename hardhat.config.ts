83839c19a9cb598f6b595f5c86d0dbee1ea9a9de
RafaCypherpunk <rafafuentesrangel@outlook.com>
2022-06-20 11:14:58 -0500

    hardhat.config.ts config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,35 +9,25 @@ import "solidity-coverage";
 
 dotenv.config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
+    mumbai: {
+      url: process.env.ALCHEMY_MUMBAI_URL,
       accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+        [process.env.PRIVATE_KEY_MUMBAI!]
     },
   },
   gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
+    gasPrice: 21,
+    enabled: true,
+    token: 'ETH',
+    gasPriceApi: "https://api.etherscan.io/api?module=proxy&action=eth_gasPrice",
+    showTimeSpent: true,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY
+  }
 };
+
 
 export default config;
